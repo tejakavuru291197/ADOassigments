0398aa9e41fc144258ece76f08708de35895bf73
tejakavuru291197 <102531123+tejakavuru291197@users.noreply.github.com>
2022-05-06 15:49:20 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="PKRtravels.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        Boarding Point&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList1" runat="server" Height="17px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="184px">
+        </asp:DropDownList>
+        <p>
+            Travel Date&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Amount&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Rating&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Save Bus Data" />
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PKRtravels
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //string cs = "data source =.; database =BooKMyBUS; integrated security=SSPI ";
+        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=BooKMyBUS;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into BusInfo values ('" + DropDownList1 + "','" + TextBox1 + "','" + TextBox2 + "','" + TextBox3 + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand conn =new SqlCommand("select * from BusInfo");
+            SqlDataAdapter da = new SqlDataAdapter(conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DropDownList1.DataTextField = ds.Tables[0].Columns["BoardingPoint"].ToString();
+            DropDownList1.DataValueField = ds.Tables[0].Columns["BusID"].ToString();
+            DropDownList1.DataSource = ds.Tables[0];
+            DropDownList1.DataBind();
+
+        }
+    }
+}
